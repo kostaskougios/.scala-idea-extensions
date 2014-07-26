939069b7c34fbfdbcb34cec22d954bf6cf7dd490
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2014-07-26 19:06:19 +0100

    change tracker

--- src/main/scala/compilation/ProjectCompiled.scala
+++ src/main/scala/compilation/ProjectCompiled.scala
@@ -16,15 +16,19 @@ class ProjectCompiled extends ProjectCompilationListener with VFSChangeListener
 	private val changeTracker = ChangeTracker.empty
 
 	override def success(context: CompileContext) = {
-		val project = context.getProject
-		val path = project.getBasePath
+		val affectedModules = context.getCompileScope.getAffectedModules
+		for (module <- affectedModules) {
+			val path = module.getModuleFile.getParent.getPath
 
-		val result = Mvn(path, Array("clean", "package", "-DskipTests"))
-		if (result.returnCode != 0) EventLog.error(this, s"mvn returned with status code ${result}", null)
-		EventLog.trace(this, result.log)
+			val result = Mvn(path, Array("clean", "package", "-DskipTests"))
+			if (result.returnCode != 0) EventLog.error(this, s"mvn returned with status code ${result}", null)
+			EventLog.trace(this, result.log)
+		}
 	}
 
 	override def contentsChanged(event: VirtualFileEvent) = {
+		//		ProjectManager.getInstance().
+		//		ModuleUtil.findModuleForFile()
 		//		changeTracker.track(event.getFile.getFileSystem.)
 	}
 }
